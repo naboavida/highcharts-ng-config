d7e7963838e24867464831f1b04e82ea15d66dab
Nuno Alexandre Boavida <ASUS@ASUSPC.(none)>
2015-09-17 16:34:17 +0100

    added highcharts to the example

--- examples/js/controllers.js
+++ examples/js/controllers.js
@@ -1,29 +1,83 @@
 'use strict';
 
-myapp.controller('StructureCtrl', function($scope, JsonService){
+myapp.controller('StructureCtrl', function($scope, JsonService, StaticJsonService){
 
 	// JsonService.get(function(data){
 	// 	console.log("got data");
 	// });
 	// we should read from the json but got issues with CORS above
 	
-	var configStructure = JSON.parse(
-		JSON.stringify(
-			{
-				"options": {
-					"chart": {
-						"type": ["bar", "line"],
-						"marginRight": [0, 1024]
-					}
-				}
-			}
-		)
-	);
+	var configStructure = StaticJsonService;
 
+	// $scope.highchartsNG = {};
+	$scope.highchartsNG = {
+        options: {
+            chart: {
+                type: 'line',
+                marginRight: 0
+            },
+            plotOptions: {
+                series: {
+                  stacking: '',
+                  marker: {
+                    enabled: true
+                  }
+                }
+              }
+        },
+        series: [{
+            data: [10, 15, 12, 8, 7]
+        }],
+        title: {
+            text: 'Hello',
+        },
+        yAxis: {
+          title: {
+            text: null
+          },
+          startOnTick: false
+        },
+        loading: false
+    };
 
 
 	$scope.chartTypes = configStructure.options.chart.type;
+	$scope.rightMarginDomain = configStructure.options.chart.marginRight;
+	$scope.symbolsEnabled = configStructure.options.plotOptions.series.marker.enabled;
+	$scope.startOnTickValues = configStructure.yAxis.startOnTick;
 
-	
+	$scope.chartTitleEnabled = true;
+	$scope.yAxisTitleEnabled = true;
+	var auxTitle = "";
+	var auxYTitle = "";
+
+	$scope.debugChartConfig = true;
+
+	$scope.$watch('chartTitleEnabled', function(newValue, oldValue, scope) {
+		if(newValue != undefined && oldValue != undefined && newValue != oldValue){
+			if(newValue == false){
+				auxTitle = $scope.highchartsNG.title.text;
+				$scope.highchartsNG.title.text = null;
+			} else {
+				$scope.highchartsNG.title.text = auxTitle;
+			}
+		}
+	});
+
+	$scope.$watch('yAxisTitleEnabled', function(newValue, oldValue, scope) {
+		if(newValue != undefined && oldValue != undefined && newValue != oldValue){
+			if(newValue == false){
+				auxYTitle = $scope.highchartsNG.yAxis.title.text;
+				$scope.highchartsNG.yAxis.title.text = null;
+			} else {
+				$scope.highchartsNG.yAxis.title.text = auxYTitle;
+			}
+		}
+	});
+
+
+	$scope.applyChartConfig = function(){
+		console.log("applyChartConfig");
+	}
 	
 })
--- examples/js/services.js
+++ examples/js/services.js
@@ -4,4 +4,28 @@ angular.module('myapp.services', ['ngResource'])
   .factory('JsonService', function($resource) {
     // return $resource('../json/highchartsStructure.json');
     return $resource('highchartsStructure.json');
-  });
+  })
+  .factory('StaticJsonService', function(){
+    return JSON.parse(
+      JSON.stringify(
+        {
+          "options": {
+            "chart": {
+              "type": ["line", "bar", "pie"],
+              "marginRight": [0, 1024]
+            },
+            "plotOptions": {
+              "series": {
+                "marker": {
+                  "enabled": [true, false]
+                }
+              }
+            }
+          },
+          "yAxis": {
+            "startOnTick": [true, false]
+          }
+        }
+      )
+    );
+  })
